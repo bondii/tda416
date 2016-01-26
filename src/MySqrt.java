6a80239ec1b12e7256f372dd7b7e0bf0f356d012
Pontus Eriksson <pontus_eriksson@live.com>
2016-01-26 17:05:53 +0100

    Renamed and added required empty methods.

---
+++ src/MySqrt.java
@@ -0,0 +1,17 @@
+public class MySqrt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static double mySqrtLoop(double x, double epsilon) {
+
+        return 0;
+    }
+
+    public static double mySqrtRecurse(double x, double epsilon) {
+
+        return 0;
+    }
+
+}
