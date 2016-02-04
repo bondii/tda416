8ae28a0b3213e650ec819eedc4a63362c62d3ab5
Pontus Eriksson <pontus_eriksson@live.com>
2016-02-04 13:47:00 +0100

    Added PDF and updating of queue to algorithm b.

--- src/Lab2b.java
+++ src/Lab2b.java
@@ -14,7 +14,7 @@ public class Lab2b {
             size++;
         }
 
-        PriorityQueue queue = new PriorityQueue();//size);
+        PriorityQueue queue = new PriorityQueue();//size); // sorted for value
 
         DLList.Node node = list.getFirst();
         node = node.getNext(); // skipping first node since it should never be removed
@@ -26,21 +26,25 @@ public class Lab2b {
             node = node.getNext();
         }
 
-        while(size > k) {
+        while(size > k) { // removing elements with lowest value until k elements left
             DLList.Node temp = (DLList.Node) queue.poll(); // retrieves and removes the head of the queue, which is the "smallest" Node/Point
 
             if (temp.getPrev().getPrev() != null) {
+                queue.remove(temp.getPrev()); // removing from queue and then adding again, in order to update new val
                 ((Point) temp.getPrev().elt).setVal(calcVal((Point) temp.getPrev().elt, (Point) temp.getPrev().getPrev().elt, (Point) temp.getNext().elt));
+                queue.add(temp.getPrev());
             }
             if (temp.getNext().getNext() != null) {
+                queue.remove(temp.getNext()); // removing from queue and then adding again, in order to update new val
                 ((Point) temp.getNext().elt).setVal(calcVal((Point) temp.getNext().elt, (Point) temp.getPrev().elt, (Point) temp.getNext().getNext().elt));
+                queue.add(temp.getNext());
             }
 
-            list.remove(temp);
-            size--;
+            list.remove(temp); // removing node from list
+            size--; // updating size
         }
 
-        double[] newArray = new double[size*2];
+        double[] newArray = new double[size*2]; // creating return array
         DLList.Node temp = list.getFirst();
         for (int i = 0; i < size; i++) {
             newArray[i*2] = ((Point) temp.elt).getX();
